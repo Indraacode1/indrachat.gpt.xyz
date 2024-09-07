41dc7233b803e4286d797291ee720babe36e1a12
Indraacode1 <udinlatif1234@gmail.com>
2024-09-07 18:32:09 +0700

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,11 @@
+// Function to fetch GPT response
+async function fetchGPTResponse(userInput) {
+    const response = await fetch(`${gptApiUrl}?text=${encodeURIComponent(userInput)}&apikey=${apiKey}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    const data = await response.json();
+    return data.data;
+}
